74c1caf488031ae2b9c04a98de2c77d8962b05e0
ineffable-key-9064 <cristianlzambrano@outlook.com>
2019-09-10 01:51:13 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,6 +1,11 @@
-
-  
 describe '#square_array' do
+  def square_array(numbers)
+    result = []
+    for numbers.each do |number|
+      result.push(number**2)
+    end
+    return result
+  end
   it 'does not call on collect/map or inject' do
     numbers = [1,2,3]
     expect(numbers).to_not receive(:collect)
